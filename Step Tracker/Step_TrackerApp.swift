f90b7757941d49e73e38ae65fc3f38dd1ef62fb1
cnturk-kdot <f7y8s2wg7z@privaterelay.appleid.com>
2025-10-15 14:23:40 -0700

    Created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Cedric Turk on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
